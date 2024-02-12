221617baaa6f5081430f0f00891f53648ac69b54
Ahmet Esad Barut <dwaebtdt@gmail.com>
2024-02-12 08:34:59 +0300

    Update page.jsx

--- web/app/logistics/page.jsx
+++ web/app/logistics/page.jsx
@@ -1,11 +1,11 @@
 import Location from "@/components/About/Location";
-import Fees from "@/components/Fees";
+// import Fees from "@/components/Fees";
 
 const LogisticsPage = () => {
   return (
     <div className="mt-16">
       <Location />
-      <Fees />
+      {/* <Fees /> */}
     </div>
   );
 };

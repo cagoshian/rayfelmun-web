f73c83212c9fb1025c74731b839c6e1ab1193b7f
Ahmet Esad Barut <dwaebtdt@gmail.com>
2024-02-21 09:24:06 +0300

    Update page.jsx

--- web/app/hcc/page.jsx
+++ web/app/hcc/page.jsx
@@ -9,11 +9,11 @@ const CABINET = () => {
             <div className="w-full px-4 lg:w-8/12">
               <div>
                 <h2 className="mb-8 text-3xl text-center font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight">
-                  HCC
+                  HCC: Historical Crisis Cabinet
                 </h2>
                 <div>
                   <p className="mb-10 text-base text-center font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed lg:text-base lg:leading-relaxed xl:text-lg xl:leading-relaxed">
-                    Chernobyl Disaster
+                    Agenda Item: Chernobyl Disaster
                   </p>
                   <div className="mb-10 w-full overflow-hidden rounded">
                     <div className="relative aspect-[97/60] w-full sm:aspect-[97/44]">
@@ -25,21 +25,9 @@ const CABINET = () => {
                       />
                     </div>
                   </div>
-                  {/* <p className="mb-8 text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed lg:text-base lg:leading-relaxed xl:text-lg xl:leading-relaxed">
-                    From the moment he assumed the presidency in 1993, his
-                    actions have made him one of America's most memorable
-                    presidents up until his last year in the office, 2001.
-                    During Bill Clinton's presidency, the U.S. experienced one
-                    of the most prosperous economic periods in its entire
-                    history. He had one of the lowest unemployment rates in
-                    history, the lowest inflation in decades, the highest number
-                    of homeowners, the lowest crime rate, and the strongest
-                    people economically. "It's the economy, stupid!" was the
-                    slogan he used to attract public attention in his campaigns.
-                    If you're determined to take big steps to make America the
-                    best it can be, just the way Clinton did it himself, this
-                    crisis cabinet is the place for you!
-                  </p> */}
+                  <p className="mb-8 text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed lg:text-base lg:leading-relaxed xl:text-lg xl:leading-relaxed">
+                    In the intriguing Historical Crisis Cabinet, participants are poised to delve into the complexities of the Chernobyl Disaster. Over the span of three days, delegates will not only grapple with the challenge of averting potential dangers at the Chernobyl Nuclear Power Plant but also tackle a myriad of issues sent from the crisis room. It's an immersive experience that invites individuals to actively engage in problem-solving, making it a unique opportunity for those with a keen interest in history, crisis management, and decision-making. If you're passionate about exploring the intricacies of historical events and contributing to solutions, don't miss the chance to apply and become an integral part of this captivating cabinet experience!
+                  </p>
                 </div>
               </div>
             </div>

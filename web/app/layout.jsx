dbd08281a4ebb62517a432fac704bb390204f7e5
cagoshian <cagankota@gmail.com>
2025-11-30 21:45:05 +0300

    remove redundant head file

--- web/app/layout.jsx
+++ web/app/layout.jsx
@@ -1,12 +1,8 @@
-"use client";
-
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Head from "./head";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Providers } from "./providers";
 import "../styles/index.css";
-
 import localFont from "next/font/local";
 
 const inter = localFont({
@@ -15,10 +11,17 @@ const inter = localFont({
   style: "normal",
 });
 
+export const metadata = {
+  title: "Rayfelmun'25",
+  description: "Rayfelmun'25",
+  icons: {
+    icon: "/images/favicon.ico",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html suppressHydrationWarning lang="en" className={inter.className}>
-      <Head />
       <body className="bg-black">
         <Providers>
           <Header />

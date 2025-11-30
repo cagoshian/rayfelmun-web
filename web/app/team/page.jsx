9b7417db848759101a45528b9047f1f603124ecd
cagoshian <cagankota@gmail.com>
2025-11-30 22:18:04 +0300

    commonize and cleanup team page and photos

--- web/app/team/page.jsx
+++ web/app/team/page.jsx
@@ -1,24 +1,11 @@
-import React from "react";
-import teamData from "@/components/Team/TeamData";
-import TeamMember from "@/components/Team/TeamMember";
+import Team from "@/components/Team";
 
-const Team = () => {
+const TeamPage = () => {
   return (
-    <section className="pt-[120px] pb-[120px]">
-      <div className="container">
-        <div className="-mx-4 flex flex-wrap justify-center">
-          {teamData.map((member) => (
-            <div
-              key={member.id}
-              className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
-            >
-              <TeamMember member={member} />
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
+    <>
+      <Team />
+    </>
   );
-}
+};
 
-export default Team;
+export default TeamPage;

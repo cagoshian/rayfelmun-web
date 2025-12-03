0a5e38b51857368be0726a2d96386a8fa45206aa
cagoshian <cagankota@gmail.com>
2025-12-04 00:00:19 +0300

    update legal pdf & unesco photo

--- web/data/committees.js
+++ web/data/committees.js
@@ -2,7 +2,7 @@ export const committeeData = {
 	unesco: {
 		title: "UNESCO",
 		subTitle: "Cultural Heritage and World Heritage Sites",
-		imageSrc: "/images/unesco.jpg",
+		imageSrc: "/images/unesco.jpeg",
 		description: "Within the esteemed United Nations Educational, Scientific and Cultural Organization committee, delegates will be discussing the importance of World Heritage Sites, the unique aspects of their cultural heritages, and the international organizations established to cover these valuable treasures.",
 		pdfLink: "/pdf/unesco-guide.pdf"
 	},

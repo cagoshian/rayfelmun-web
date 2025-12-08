e6e1719909eecbdb678f1aa1b9754f95b236ff4c
yusu <yusu@duck.com>
2025-12-09 02:25:29 +0300

    chore(style): apply various styling improvements

--- web/components/Footer/index.jsx
+++ web/components/Footer/index.jsx
@@ -1,87 +1,68 @@
 import Image from "next/image";
 import Link from "next/link";
-import { InstagramIcon, TikTokIcon } from "@/components/Common/Icons";
 import footerData from "@/data/footerData";
 
 const Footer = () => {
-  return (
-    <footer className="relative z-10 bg-gradient-to-b from-transparent to-primary pt-6 md:pt-10 lg:pt-14">
-      <div className="container">
-        <div className="-mx-4 flex flex-wrap">
-	        
-          <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12 mb-12 lg:mb-16">
-            <div className="max-w-[360px]">
-	            
-              <div className="mb-8 flex flex-row items-center gap-4">
-                <Link href="/" className="inline-block">
-                  <Image src="/images/logo.png" alt="Rayfelmun Logo" width={112} height={24} className="w-28 block" />
-                </Link>
-                <div className="h-8 w-[1px] bg-body-color/30"></div>
-                <Link href="/" className="inline-block">
-                  <Image src="/images/UN_logo.png" alt="UN Logo" width={112} height={24} className="w-28 block" />
-                </Link>
-              </div>
+    return (
+        <footer className="relative z-10 bg-gradient-to-b from-transparent to-primary pt-6 md:pt-10 lg:pt-14">
+            <div className="container">
+                <div className="-mx-4 flex flex-wrap">
+                    <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12 mb-2 lg:mb-16">
+                        <div className="max-w-[360px]">
+                            <div className="mb-8 flex flex-row items-center gap-4">
+                                <Link href="/" className="inline-block">
+                                    <Image src="/images/logo.png" alt="Rayfelmun Logo" width={112} height={24}
+                                           className="w-24 block"/>
+                                </Link>
 
-              <div className="flex items-center">
-                <a
-                  href="https://www.instagram.com/rayfelmun/"
-                  target="_blank"
-                  rel="nofollow noopener"
-                  aria-label="Instagram"
-                  className="mr-6"
-                >
-                  <InstagramIcon />
-                </a>
-	              
-	              <a
-                  href="https://www.tiktok.com/@rayfelmun0/"
-                  target="_blank"
-                  rel="nofollow noopener"
-                  aria-label="TikTok"
-                  className="mr-6"
-                >
-                  <TikTokIcon />
-                </a>
-              </div>
-            </div>
-          </div>
-	        
-          {footerData.map((section, index) => (
-            <div key={index} className={`w-full px-4 sm:w-1/2 md:w-1/2 lg:w-4/12 ${index === 0 ? 'xl:w-2/12' : 'xl:w-3/12'}`}>
-              <div className="mb-12 lg:mb-16">
-                <h2 className="mb-10 text-xl font-bold text-white">{section.title}</h2>
-                <ul>
-                  {section.links.map((link, idx) => (
-                    <li key={idx}>
-                      <Link href={link.href} className="mb-4 inline-block text-base font-medium text-body-color hover:text-white transition-colors">
-                        {link.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+                                <div className="h-22 w-1/360 bg-white/70"></div>
 
-      <div className="py-3">
-        <div className="container flex justify-center">
-          <p className="text-center text-base text-white">
-            We are{" "}
-            <a
-              href="https://github.com/cagoshian/rayfelmun-web"
-              target="_blank"
-              rel="nofollow noopener"
-              className="text-body-color hover:text-white transition-colors underline"
-            >
-              open source!
-            </a>
-          </p>
-        </div>
-      </div>
-    </footer>
-  );
+                                <Link href="/" className="inline-block">
+                                    <Image src="/images/UN_logo.png" alt="UN Logo" width={112} height={24}
+                                           className="w-24 block"/>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    {footerData.map((section, index) => (
+                        <div key={index}
+                             className={`w-full px-4 sm:w-1/2 md:w-1/2 lg:w-4/24 ${index === 0 ? 'xl:w-4/24' : 'xl:w-4/24'}`}>
+                            <div className="mb-4 lg:mb-4">
+                                <h2 className="mb-3 text-xl font-bold text-white">{section.title}</h2>
+
+                                <ul>
+                                    {section.links.map((link, idx) => (
+                                        <li key={idx}>
+                                            <Link href={link.href}
+                                                  className={link.noStyle ? "" : "mb-1 inline-block text-base font-medium text-[#ddd] hover:text-white transition-colors ease-in duration-200"}>
+                                                {link.name}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="py-3">
+                <div className="container flex justify-center">
+                    <p className="text-center text-base text-white">
+                        &copy; {new Date().getFullYear()} • Open Source on <a
+                            href="https://github.com/cagoshian/rayfelmun-web"
+                            target="_blank"
+                            rel="nofollow noopener"
+                            className="text-gray-300 hover:text-white transition-colors ease-in duration-200 underline"
+                        >
+                            GitHub
+                        </a> • Apache License 2.0
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- web/components/Header/index.jsx
+++ web/components/Header/index.jsx
@@ -24,13 +24,13 @@ const Header = () => {
     <header
       className={`header top-0 left-0 z-40 flex w-full items-center transition-all duration-300 ${
         sticky
-          ? "fixed z-[9999] bg-primary/50 shadow-sticky backdrop-blur-lg py-2 lg:py-0"
+          ? "fixed z-10 bg-primary/50 shadow-sticky backdrop-blur-lg py-2 lg:py-0"
           : "absolute bg-transparent py-4 lg:py-0"
       }`}
     >
-      <div className="container relative -mx-4 flex items-center justify-between">
+      <div className="w-full relative -mx-4 flex items-center justify-between">
 	      
-        <div className="w-auto px-4 xl:mr-12">
+        <div className="w-auto px-10 xl:mr-12">
           <Link href="/" className={`block w-full ${sticky ? "py-2" : "py-2 lg:py-5"}`}>
             <div className="flex items-center gap-3">
               <Image
@@ -50,17 +50,17 @@ const Header = () => {
         <div className="flex w-full items-center justify-end px-4">
           <button
             onClick={() => setNavbarOpen(!navbarOpen)}
-            className="absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
+            className="absolute right-4 top-1/2 block -translate-y-1/2 cursor-pointer bg-blue-800/20 rounded-lg px-3 py-[6px] min-[1160px]:hidden"
             aria-label="Mobile Menu Toggle"
           >
-            <span className={`relative my-1.5 block h-0.5 w-[30px] bg-white transition-all ${navbarOpen ? "top-[7px] rotate-45" : ""}`} />
-            <span className={`relative my-1.5 block h-0.5 w-[30px] bg-white transition-all ${navbarOpen ? "opacity-0" : ""}`} />
-            <span className={`relative my-1.5 block h-0.5 w-[30px] bg-white transition-all ${navbarOpen ? "-top-[8px] -rotate-45" : ""}`} />
+            <span className={`relative my-1.5 block h-0.5 w-[25px] bg-white transition-all ${navbarOpen ? "top-[7px] rotate-45" : ""}`} />
+            <span className={`relative my-1.5 block h-0.5 w-[25px] bg-white transition-all ${navbarOpen ? "opacity-0" : ""}`} />
+            <span className={`relative my-1.5 block h-0.5 w-[25px] bg-white transition-all ${navbarOpen ? "-top-[8px] -rotate-45" : ""}`} />
           </button>
 	        
           <nav
-            className={`absolute right-0 z-30 w-[250px] rounded-2xl border-[.5px] border-body-color/20 bg-dark py-4 px-6 duration-300 lg:visible lg:static lg:w-auto lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 ${
-              navbarOpen ? "visible top-full opacity-100" : "invisible top-[120%] opacity-0"
+            className={`absolute right-0 z-30 w-[250px] rounded-2xl border-[.5px] border-body-color/20 bg-blue-900 py-4 px-6 duration-300 lg:visible lg:static lg:w-auto lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 ${
+              navbarOpen ? "visible top-full opacity-100" : "hidden min-[1160px]:flex invisible top-[120%] opacity-0"
             }`}
           >
             <ul className="block lg:flex lg:space-x-12">
@@ -77,12 +77,12 @@ const Header = () => {
                         className="flex cursor-pointer items-center justify-between py-2 text-base text-white/70 group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
                       >
                         {item.title}
-                        <span className="pl-3">
+                        <span className={`pl-3 transition-rotate ease-in duration-200 ${openIndex === index ? "rotate-x-180" : "rotate-x-0"}`}>
                           <ChevronDownIcon />
                         </span>
                       </p>
                       <div
-                        className={`submenu relative left-0 top-full rounded-2xl bg-dark transition-[top] duration-300 group-hover:opacity-100 lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
+                        className={`submenu relative transition-all left-0 top-full rounded-lg bg-blue-800 duration-300 group-hover:opacity-100 lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
                           openIndex === index ? "block" : "hidden"
                         }`}
                       >
@@ -90,7 +90,7 @@ const Header = () => {
                           <Link
                             key={subItem.id || subItem.path}
                             href={subItem.path}
-                            className="block rounded py-2.5 text-sm text-white/70 hover:text-white lg:px-3"
+                            className="block ml-4 rounded py-2.5 text-sm text-white/70 hover:text-white lg:px-3"
                           >
                             {subItem.title}
                           </Link>

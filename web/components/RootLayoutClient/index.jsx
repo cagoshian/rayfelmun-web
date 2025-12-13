f072c1dab2d844e03c5c8bb4ce8f4be356130500
cagoshian <cagankota@gmail.com>
2025-12-13 22:44:41 +0300

    fix padding

--- web/components/RootLayoutClient/index.jsx
+++ web/components/RootLayoutClient/index.jsx
@@ -9,7 +9,7 @@ export default function RootLayoutClient({ children }) {
   return (
     <Providers>
       <Header />
-      <section className="pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]">
+      <section className="pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px] min-h-196">
         <div className="container">
           <div className="flex flex-col items-center">
             {children}
